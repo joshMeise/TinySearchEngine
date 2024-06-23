fa6bb4ed8b2ecc5f07896d3494f8d5afa425e9ee
Josh Meise <joshua.m.meise.24@dartmouth.edu>
2024-06-22 21:21:16 -0400

    scripts added and readme fixed

---
+++ scripts/makeall.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
+# check arguments
+if [ $# != 1 ] && [ $# != 0 ]; then
+		echo "usage: makeall.sh [ -s ]"
+		echo "   -h : help"
+		exit
+fi
+
+# quiet by default
+MODE=-s
+if [ $# == 1 ] && [ $1 == "-s" ]; then
+		unset MODE
+fi
+
+# revert to the tse directory
+cd ..
+
+# make all of the components
+for DIR in utils crawler indexer querier parallel_indexer
+do
+
+		if [ -d ./${DIR} ] ; then
+				pushd ./${DIR}
+				echo [$DIR]
+				make ${MODE} clean
+				make ${MODE}
+				popd
+		fi
+
+done
+
